9ed7867fcaa26e33b31feade80394e48f300c5c6
ARCADIA\elizaveta.sharkova <elizaveta.sharkova@arcadia.spb.ru>
2019-02-28 23:34:09 +0300

    Download data from DB table

--- react-backend/routes/DBconnection.js
+++ react-backend/routes/DBconnection.js
@@ -2,29 +2,17 @@ var express = require('express');
 var router = express.Router();
 var mysql      = require('mysql');
 
-
-let tableName = 'categories';
-  const connection = mysql.createConnection({
-        host     : 'localhost',
-        user     : 'root',
-        password : 'password',
-        database : 'northwind',
-        port: 3306
-    });
-
+    
 /* GET tables listing. */
 router.get('/', function(req, res, next) {
-  
-  let select = 'SHOW TABLES FROM ' + connection.config.database;        
-  getDataBySelect(select);  
+  let select = 'SHOW TABLES FROM northwind';        
+  getDataBySelect(res, select);
  
 });
 
-router.get('/table', function(req, res, next) {
-  
-  let select = 'SELECT * FROM northwind.' + tableName; 
-    
-  getDataBySelect(select);  
+router.get('/:tableName', function(req, res, next) {
+  let select = 'SELECT * FROM northwind.' + req.params.tableName;   
+  getDataBySelect(res, select);
  
 });
 
@@ -32,13 +20,19 @@ module.exports = router;
 
 
 
-function getDataBySelect(select){
-  
+function getDataBySelect(response, select){
+  const connection = mysql.createConnection({
+    host     : 'localhost',
+    user     : 'root',
+    password : 'password',
+    database : 'northwind',
+    port: 3306
+});
 
   connection.connect();
   connection.query(select, function (error, results, fields) {
       if (error) throw error;
-      res.send(results);
+      response.send(results);
   });
   connection.end();
 }
